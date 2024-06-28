5eb6f23ce6cd71d96ba220ad784f65c125abd82f
AbhishekJupalli-1409 <jupalliabhishek1409@gmail.com>
2024-06-28 18:23:45 +0530

    added movement for skills

--- script.js
+++ script.js
@@ -78,3 +78,90 @@ for (let i = 4; i < AllnavAnchortag.length; i++) {
 
 
 
+//handle scroll events on windows
+//check skills section visible
+// when visiblel fire the annimation
+//all the skills should move from left to right
+//store skill level -> hmtl with the help data attribute
+
+
+
+
+
+var Allskills = document.querySelectorAll(".skills-display #Movement");
+var skillContainer = document.querySelector("#skills")   //when visible fire animation
+console.log(Allskills);
+console.log(skillContainer);
+var bool = true;
+window.addEventListener('scroll', checkScroll);
+function initialiseSkills() {
+    for (let bar of Allskills) {
+
+        bar.style.left = "-140%";
+        bar.style.top = "-100%";
+
+    }
+    console.log("worked");
+}
+initialiseSkills();
+
+function checkScroll() {
+    //check skills section visible-->top < vport hight
+    var coordinateSkills = skillContainer.getBoundingClientRect();
+    var middleSection = coordinateSkills.top;
+    middleSection = middleSection + (coordinateSkills.bottom - coordinateSkills.top) / 2;
+
+    if (middleSection < window.innerHeight && bool) {
+
+        console.log("skills visible");
+        console.log(window.innerHeight);
+        setTimeout(() => {
+            MoveSkills();  // for delay 400ms for user experience
+        }, 400);
+
+        bool = false;
+    }
+
+
+}
+
+
+
+
+
+function MoveSkills() {
+    for (let i = 0; i < Allskills.length; i++) {
+        let movementSize = Allskills[i].getAttribute('data-movement');
+        let currentMovement = -100;
+        for (let j = 0; j < Allskills.length; j++) {
+            let intervalMovement = setInterval(function () {
+                if (currentMovement > movementSize) {
+                    clearInterval(intervalMovement);
+                    return;
+                }
+                else {
+                    currentMovement += 0.65;
+                    Allskills[i].style.left = currentMovement + "%";
+                }
+            }, 20)
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
