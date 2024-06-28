fe19c9d6d758daf94dca5c21474b81b58e173d40
AbhishekJupalli-1409 <jupalliabhishek1409@gmail.com>
2024-06-28 12:42:28 +0530

    fixed the bugs in education n experience images

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ AllnavAnchortag[0].addEventListener("click", function () {
     )
 });
 
+
+var smoothScroll;
+//for 1 -4 links
 for (let i = 1; i < 4; i++) {
     AllnavAnchortag[i].addEventListener("click", function (event) {
         event.preventDefault();
@@ -24,23 +27,30 @@ for (let i = 1; i < 4; i++) {
         var targetSectionId = AllnavAnchortag[i].textContent.trim().toLowerCase();
         var targetSection = document.getElementById(targetSectionId);
 
-        var smoothScroll = setInterval(
-            function () {
-                //putting this inside func bcoz evertime we should need new coordinate values
-                var targetSectionCoordinates = targetSection.getBoundingClientRect();
+        // smoothScroll = setInterval(scrollvertically, 15, targetSection)
 
-                if (targetSectionCoordinates.top <= 0) {
-                    clearInterval(smoothScroll);  //disabling click by redirecting action
-                    return;
-                }
-                window.scrollBy(0, 15);
-            }, 15
-        )
+        smoothScroll = setInterval(function () {
+            scrollvertically(targetSection)
+        }, 15);
 
         // console.log(targetSection);
         // console.log(targetSectionId);
     });
 }
+
+function scrollvertically(targetSection) {
+    //putting this inside func bcoz evertime we should need new coordinate values
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
+
+    if (targetSectionCoordinates.top <= 0) {
+        clearInterval(smoothScroll);  //disabling click by redirecting action
+        return;
+    }
+    window.scrollBy(0, 15);
+}
+
+
+
 for (let i = 4; i < AllnavAnchortag.length; i++) {
     AllnavAnchortag[i].addEventListener("click", function (event) {
         event.preventDefault();
